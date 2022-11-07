d8617d6d8d9c3d78a5083b47f15c51b0be1e60e4
dakshita-tripathi <96466378+dakshita-tripathi@users.noreply.github.com>
2022-11-08 02:02:09 +0530

    Added workforce and organisers class

--- Assignment1/ObjectOrientedProgramming.cpp
+++ Assignment1/ObjectOrientedProgramming.cpp
@@ -1,5 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+class organisers{
+    public:
+        string studentID;
+        string username;
+        string name;
+        int age;
+        string mobileNumber;
+        void display_basic(){
+            cout<<"The details are as follows: "<<endl;
+            cout<<"Name: "<<name<<endl;
+            cout<<"Age: "<<age<<endl;
+            cout<<"Mobile Number: "<<mobileNumber<<endl;
+        }
+}
+class organisers: public workforce{
+    public:
+        vector <string> coordinators_you_report_to;
+        vector<string> activities;
+    private:
+        string password;
+        void display(){
+            display_basic();
+            cout<<"Activities you are a part of: "<<endl;
+            for(int i=0; i<activities.size(); i++){
+            cout<<i+1<<") ";
+            cout<<activities[i]<<endl;
+            }
+            cout<<"Coordinators you report to: "<<endl;
+            for(int i=0; i<coordinators_you_report_to.size(); i++){
+            cout<<i+1<<") ";
+            cout<<coordinators_you_report_to[i]<<endl;
+            }
+        }
+}
 
 //Class of Core Members
 class coreMembers
