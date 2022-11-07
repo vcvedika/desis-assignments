986b5f9a7efc0d7e0f8408a486641d8d3ab8cfd9
Vedika Chandra <94168243+vcvedika@users.noreply.github.com>
2022-11-07 18:31:25 +0530

    Added code for Class of Core Members

---
+++ Assignment1/ObjectOrientedProgramming.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Class of Core Members
+class coreMembers
+{
+    //access specifier
+    public:
+
+    //data members
+    string coreMemberID;
+    string username;
+    string name;
+    int age;
+    string mobileNumber;
+    string areaOfSpecialization;
+    vector <string> coordinators;
+
+    //access specifier
+    private:
+
+    //data members
+    string password;
+
+    //member functions
+    void displayDetails()
+    {
+        cout<<"The details are as follows: "<<endl;
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+        cout<<"Mobile Number: "<<mobileNumber<<endl;
+        cout<<"Area of Specialization: "<<areaOfSpecialization<<endl;
+
+        cout<<"The names of the coordinators under the Core Member are: "<<endl;
+        
+        for(int i=0; i<coordinators.size(); i++)
+        {
+            cout<<i+1<<") ";
+            cout<<coordinators[i]<<endl;
+        }
+    }
+};
+
+
+int main()
+{
+    //Instantiating the classes
+}
+
