1929fcf0cf8e9fae1ba9f4db904490fadee62f54
vedika <vedika@gmail.com>
2022-11-08 16:37:34 +0530

    modified the classes

--- Assignment1/ObjectOrientedProgramming.cpp
+++ Assignment1/ObjectOrientedProgramming.cpp
@@ -1,78 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
-class organisers{
+
+class organisers
+{
     public:
         string studentID;
         string username;
         string name;
         int age;
         string mobileNumber;
-        void display_basic(){
+
+        void display_basic()
+        {
             cout<<"The details are as follows: "<<endl;
             cout<<"Name: "<<name<<endl;
             cout<<"Age: "<<age<<endl;
             cout<<"Mobile Number: "<<mobileNumber<<endl;
         }
-}
-class organisers: public workforce{
-    public:
-        vector <string> coordinators_you_report_to;
-        vector<string> activities;
+};
+
+
+class coreMembers: public organisers
+{
     private:
         string password;
-        void display(){
+
+    public:
+        string areaOfSpecialization;
+        vector <string> coordinators;
+       
+        void display()
+        {
+            display_basic();
+            cout<<"Area of Specialization: "<<areaOfSpecialization<<endl;
+
+            cout<<"The names of the coordinators under the Core Member are: "<<endl;
+
+            for(int i=0; i<coordinators.size(); i++)
+            {
+                cout<<i+1<<") ";
+                cout<<coordinators[i]<<endl;
+            }
+        }
+};
+
+
+class workforce: public organisers
+{
+    private:
+        string password;
+    
+    public:
+        vector <string> coordinators_you_report_to;
+        vector <string> activities;
+
+        void display()
+        {
             display_basic();
             cout<<"Activities you are a part of: "<<endl;
-            for(int i=0; i<activities.size(); i++){
-            cout<<i+1<<") ";
-            cout<<activities[i]<<endl;
+            for(int i=0; i<activities.size(); i++)
+            {
+                cout<<i+1<<") ";
+                cout<<activities[i]<<endl;
             }
+
             cout<<"Coordinators you report to: "<<endl;
-            for(int i=0; i<coordinators_you_report_to.size(); i++){
-            cout<<i+1<<") ";
-            cout<<coordinators_you_report_to[i]<<endl;
+            for(int i=0; i<coordinators_you_report_to.size(); i++)
+            {
+                cout<<i+1<<") ";
+                cout<<coordinators_you_report_to[i]<<endl;
             }
         }
-}
-
-//Class of Core Members
-class coreMembers
-{
-    //access specifier
-    private:
-
-    //data members
-    string password;
-    
-    //access specifier
-    public:
-
-    //data members
-    string coreMemberID;
-    string username;
-    string name;
-    int age;
-    string mobileNumber;
-    string areaOfSpecialization;
-    vector <string> coordinators;
-
-    //member functions
-    void displayDetails()
-    {
-        cout<<"The details are as follows: "<<endl;
-        cout<<"Name: "<<name<<endl;
-        cout<<"Age: "<<age<<endl;
-        cout<<"Mobile Number: "<<mobileNumber<<endl;
-        cout<<"Area of Specialization: "<<areaOfSpecialization<<endl;
-
-        cout<<"The names of the coordinators under the Core Member are: "<<endl;
-        
-        for(int i=0; i<coordinators.size(); i++)
-        {
-            cout<<i+1<<") ";
-            cout<<coordinators[i]<<endl;
-        }
-    }
 };
 
 
